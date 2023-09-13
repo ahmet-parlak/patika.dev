425c129be919f292bd6f4308758b2a0048cdfce4
ahmet-parlak <117575634+ahmet-parlak@users.noreply.github.com>
2023-09-13 17:11:15 +0300

    Node.js Hw-2

---
+++ Node.js/Hw-2/posts.js
@@ -0,0 +1,38 @@
+const posts = [
+    { user: 'John Doe', content: 'Lorem ipsum dolor sit amet, consectetur' },
+    { user: 'Jarvis Pepperspray', content: 'Lorem ipsum dolor sit amet' },
+    { user: 'Samuel Serif', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' },
+];
+
+const listPosts = () => {
+    posts.map((post) => {
+        console.log(post.user);
+        console.log(post.content);
+        console.log("----------------------------------------------------------");
+    });
+};
+
+const addPost = (newPost) => {
+    return new Promise((resolve, reject) => {
+        posts.push(newPost)
+        resolve(posts)
+    });
+};
+
+
+listPosts();
+
+(async () => {
+    try {
+        await addPost({ user: "Barry Tone", content: "Duis aute irure dolor in reprehenderit in" });
+        listPosts();
+    } catch (err) {
+        console.log(err);
+    }
+})();
+
+
+
+
+
+
